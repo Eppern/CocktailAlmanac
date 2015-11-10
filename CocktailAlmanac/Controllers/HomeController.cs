45a0e4cd039c91dde663c5123af142a91e53ed67
Eppensteiner Rene <rene.eppensteiner@gmail.com>
2015-11-10 14:47:36 +0100

    HomeViewController

--- CocktailAlmanac/Controllers/HomeController.cs
+++ CocktailAlmanac/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CocktailAlmanac.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,18 @@ namespace CocktailAlmanac.Controllers
     [RequireHttps]
     public class HomeController : Controller
     {
+        private CocktailEntities1 db = new CocktailEntities1();
+
         public ActionResult Index()
         {
-            return View();
+            HomeViewModel model = new HomeViewModel();
+            Random rndDay = new Random();
+            Random rndMonth = new Random();
+            model.Recipes = db.RECIPE.ToList();
+            model.RecipeOfTheDay = model.Recipes.OrderBy(r => rndDay.Next()).FirstOrDefault();
+            model.RecipeOfTheMonth = model.Recipes.OrderBy(r => rndMonth.Next()).FirstOrDefault();
+            model.BestRated = db.RECIPE.OrderByDescending(r => r.Rating).FirstOrDefault();
+            return View(model);
         }
 
         public ActionResult About()
---
+++ CocktailAlmanac/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CocktailAlmanac.Models
+{
+    public class HomeViewModel
+    {
+        public List<RECIPE> Recipes { get; set; }
+        public RECIPE RecipeOfTheMonth { get; set; }
+        public RECIPE RecipeOfTheDay { get; set; }
+        public RECIPE BestRated { get; set; }
+    }
+}
